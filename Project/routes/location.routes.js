4ebc77c1e51fea89e2ebcdb3e66d1343e42a5383
Ivana-Lida Heintze <lydiaive@gmail.com>
2022-11-07 17:49:26 +0100

    location list created

--- Project/routes/location.routes.js
+++ Project/routes/location.routes.js
@@ -3,10 +3,19 @@ const router = express.Router();
 
 // ℹ️ Handles password encryption
 const mongoose = require("mongoose");
+
+const Location = require("../models/Location.model")
 const isLoggedIn = require("../middleware/isLoggedIn");
 
-router.get("/", isLoggedIn, (req, res) => {
-    res.render("location/main-feed");
+router.get("/", isLoggedIn, async(req, res) => {
+
+  try {
+    const locationDb = await Location.find()
+    res.render("location/main-feed", {locationDb});
+    console.log(locationDb)
+  } catch (error) {
+    console.log(error)
+  }
   });
 
 
